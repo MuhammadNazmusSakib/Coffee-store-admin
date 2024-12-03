61b8c9c19cb9ddc9e283fe315c84c7c3ac77bf34
MuhammadNazmusSakib <sakib21feb@gmail.com>
2024-12-03 19:29:45 +0600

    env setup

--- src/Components/SignIn&SignUp/Firebase/firebase.init.js
+++ src/Components/SignIn&SignUp/Firebase/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBSFiHNPAwMNTHv9aMC9ZRxoOTErNfrgK8",
-  authDomain: "coffee-store-client-24b21.firebaseapp.com",
-  projectId: "coffee-store-client-24b21",
-  storageBucket: "coffee-store-client-24b21.firebasestorage.app",
-  messagingSenderId: "339513298913",
-  appId: "1:339513298913:web:d656c0b1f67c035efc0ca0"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
